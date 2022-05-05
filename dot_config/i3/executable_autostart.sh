c40b7ab5be5d0011361e55d19abefbf53cb4a90d
Alex Vanderpot <alex@vanderpot.com>
2022-05-05 11:34:20 -0700

    touchpad

--- dot_config/i3/executable_autostart.sh
+++ dot_config/i3/executable_autostart.sh
@@ -6,6 +6,7 @@ pasystray &
 #redshift-gtk -t 6500:4000 -l geoclue2 &
 /usr/lib/geoclue-2.0/demos/agent &
 xinput set-button-map "Synaptics TM3289-002" 1 1 3 4 5 6 7 &
+xinput set-button-map "ELAN067A:00 04F3:3197 Touchpad" 1 1 3 4 5 6 7 8 9 10 11 12 &
 autorandr -c &
 #parcellite -n &
 thunar --daemon &
